c5d6b24ded1f154ecbc0cb6001a23d987f026b61
Fernando Schuab <fschuab@gmail.com>
2026-03-28 18:39:21 -0300

    referencia professor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import CadastrarAlunos from "./pages/CadastrarAlunos";
+import Alunos from './pages/Alunos';
+import Biblioteca from './pages/Biblioteca';
+import Dashboard from './pages/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+
+        <Header/>
+
+        <main>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/cadastarAlunos' element={<CadastrarAlunos/>} />
+            <Route path='/Alunos' element={<Alunos/>} />
+            <Route path='/Biblioteca' element={<Biblioteca/>} />
+            <Route path='/Dashboard' element={<Dashboard/>} />
+          </Routes>
+        </main>
+
+        <Footer/>
+
+      </BrowserRouter>
   );
 }
 
